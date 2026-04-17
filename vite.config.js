9c04d113c95265e9ae8c4317e5b863cb021ba62d
rossy1668 <cristo26111@gmail.com>
2026-04-17 17:12:08 -0500

    cambio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGithubPages = process.env.GH_PAGES === 'true'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Pwc_Trivia/',
+  base: isGithubPages ? '/Pwc_Trivia/' : '/',
 })
